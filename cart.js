2fe9ef95220adfba888b16cd6b40ab00a8bf5e9e
Nishant Kaushal <101548649+nishant0708@users.noreply.github.com>
2024-05-12 19:11:54 +0530

    all pages done except cart

--- cart.js
+++ cart.js
@@ -1,8 +1,5 @@
 document.addEventListener("DOMContentLoaded", function() {
-  // Retrieve cart items from localStorage
   const cartItems = JSON.parse(localStorage.getItem('cartItems')) || [];
-
-  // Display cart items on the cart page
   const cartItemsContainer = document.getElementById('cart-items');
   const cartTotalElement = document.getElementById('cart-total');
   let total = 0;
@@ -10,39 +7,37 @@ document.addEventListener("DOMContentLoaded", function() {
   cartItems.forEach(function(item, index) {
     const itemElement = document.createElement('div');
     itemElement.classList.add('cart-item');
+    itemElement.style.display = 'flex'; // Apply flex display to the cart item
+    itemElement.style.width = '100%';
+
     itemElement.innerHTML = `
-      <p>Name: ${item.name}</p>
-      <p>Price: Rs${item.price.toFixed(2)}</p>
-      <p>Quantity: ${item.quantity}</p>
-      <button class="remove-from-cart" data-index="${index}">Remove</button>
+      <img src="${item.imgUrl}" width="100px" height="100px" alt="${item.name}">
+      <div>
+        <p><strong>${item.name}</strong></p>
+        <p>Price: Rs${item.price.toFixed(2)}</p>
+        <p>Quantity: ${item.quantity}</p>
+      </div>
+      <button class="remove-from-cart" data-index="${index}"><i class="fas fa-times"></i></button>
     `;
+
     cartItemsContainer.appendChild(itemElement);
 
     total += item.price * item.quantity;
   });
 
-  // Update total
-  cartTotalElement.textContent = `Rs${total.toFixed(2)}`;
+  cartTotalElement.textContent = `Total: Rs${total.toFixed(2)}`;
 
-  // Add event listeners to remove buttons
   const removeFromCartButtons = document.querySelectorAll('.remove-from-cart');
   removeFromCartButtons.forEach(function(button) {
     button.addEventListener('click', function() {
       const index = parseInt(button.dataset.index);
-      // Decrease quantity by 1 if greater than 1
       if (cartItems[index].quantity > 1) {
         cartItems[index].quantity -= 1;
       } else {
-        // Remove the item if quantity is 1 or less
         cartItems.splice(index, 1);
       }
-      // Update localStorage
       localStorage.setItem('cartItems', JSON.stringify(cartItems));
-      // Refresh the page to reflect the changes
-      location.reload();
+      location.reload(); // Refresh the page to reflect changes
     });
   });
-
-  // Add event listener to "Order Now" button
-
 });
